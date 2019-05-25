e29a079db18db8f2971eb021aec6abe14e519f67
Hayg Astourian <hayg@meraki.com>
2019-05-24 18:37:04 -0700

    Server + demo client v0.01
    Multithreaded server using blocking network calls. Server has a thread
    that updates the game state every 100ms, then threads for listening on
    API (tcp) server and Realtime (udp) server. API server is just used for
    registering clients right now.

--- src/client/main_demo_client.cpp
+++ src/client/main_demo_client.cpp
@@ -7,83 +7,134 @@
 #include <chrono>
 #include <ctime>
 
-int my_group_id;
+// dedup these into shared header file
+enum class APICommand { register_client, group, ungroup };
+enum class RealtimeCommand { move, fetch_state };
+
+sf::Uint32 my_client_id;
 sf::Uint32 curr_tick;
-sf::Uint32 serv_x_pos;
-sf::Uint32 serv_y_pos;
+// these values should be user input instead of hard-coded
+sf::Uint32 x_dir = 1;
+sf::Uint32 y_dir = 1;
 
-void recv(sf::TcpSocket& s, std::mutex& mutex)
-{
-  std::cout << "Entered receive thread.\n";
-  while (true) {
-    sf::Packet packet;
-    sf::Uint32 tick;
-    sf::Uint32 group_id;
-    sf::Uint32 x_pos;
-    sf::Uint32 y_pos;
-    sf::Uint32 x_dir;
-    sf::Uint32 y_dir;
+bool is_registered = false;
 
-    std::cout << "Receive entered\n";
-    if (s.receive(packet) == sf::Socket::Done) {
-      std::cout << "Receive successful!\n";
+void read_registration_response(sf::TcpSocket* api_client) {
+  // read registration data
+  sf::Packet registration_response;
+  sf::Uint32 api_command;
+  sf::Uint32 client_id;
+  sf::Uint32 tick;
 
-      if (packet >> group_id >> tick >> x_dir >> y_dir >> x_pos >> y_pos) {
-        std::cout << "Received group_id:" << group_id << " tick:" << tick << " x:" <<  x_pos << " y:"<< y_pos << "\n";
-        // update own position to server view
-        if (my_group_id == group_id) {
-          curr_tick = tick;
-          serv_x_pos = x_pos;
-          serv_y_pos = y_pos;
-          std::cout << "Adjusting to server values for position\n";
-        }
-      }
+  if (api_client->receive(registration_response) == sf::Socket::Done) {
+    if (registration_response >> api_command >> client_id >> tick && (api_command == (sf::Uint32)APICommand::register_client)) {
+      std::cout << "Registered with ID and current tick: " << client_id << " " << tick << std::endl;
+      my_client_id = client_id;
+      curr_tick = tick;
+      is_registered = true;
     }
-    std::this_thread::sleep_for(std::chrono::seconds(1));
   }
 }
 
-void send(sf::TcpSocket& s, std::mutex& mutex)
-{
-  std::cout << "Entered sending thread.\n";
+// these will be strictly for ungroup/group/register/deregister and other Big abstraction changes
+// not sure it's really necessary, but implementing retry logic for something like ungrouping specifically
+// would be annoying. We're a lot more OK with duplication/out of order sends/dropped datagrams for stuff like moving.
+void api_client_recv(sf::TcpSocket* api_client) {}
+void api_client_send(sf::TcpSocket* api_client) {}
+
+void realtime_client_recv(sf::UdpSocket* realtime_client) {
   while (true) {
     sf::Packet packet;
-    curr_tick++;
-    std::cout << "LOCKED IN SEND.\n";
-    sf::Uint32 tick = curr_tick;
-    sf::Uint32 group_id = (sf::Uint32) my_group_id;
-    sf::Uint32 x_pos = serv_x_pos + 1;
-    sf::Uint32 y_pos = serv_y_pos + 1;
-    std::cout << "UNLOCKED IN SEND.\n";
-    sf::Uint32 x_dir = 0;
-    sf::Uint32 y_dir = 0;
-    //std::cout << "Crafting packet to send with " << group_id << tick << x_dir << y_dir << x_pos << y_pos << "\n";
-    if (packet << group_id << tick << x_dir << y_dir << x_pos << y_pos)
-    {
-      std::cout << "Sending group_id:" << group_id << " tick:" << tick << " x_dir:" << x_dir << " y_dir:" << y_dir << " x_pos:" << x_pos << " y_pos:" << y_pos  << "\n";
-      s.send(packet);
+    sf::IpAddress sender;
+    unsigned short port;
+    realtime_client->receive(packet, sender, port);
+    // fetch state updates for now
+    sf::Uint32 server_tick;
+    sf::Uint32 client_id;
+    sf::Uint32 x_pos;
+    sf::Uint32 y_pos;
+    if (packet >> server_tick) {
+      while (packet >> client_id >> x_pos >> y_pos) {
+        std::cout << "Client: " << client_id << " has position x,y: " << x_pos << " " << y_pos << std::endl;
+      }
+      // Im not sure what kind of synchronization needs to happen here.
+      // If this tick is the most up-to-date we've ever seen, maybe we set the game to it?
+      std::cout << "Client positions from server for tick: " << server_tick << std::endl;
+      curr_tick = server_tick;
+    } else {
+      std::cout << "Failed to read server tick from new packet" << std::endl;
     }
-    std::cout << "SLEEP IN SEND.\n";
-    std::this_thread::sleep_for(std::chrono::seconds(1));
+  }
+}
+
+void realtime_client_send(sf::UdpSocket* realtime_client) {
+  while (true) {
+    sf::Packet packet;
+    sf::Uint32 move_cmd = (sf::Uint32)RealtimeCommand::move;
+    if (packet << my_client_id << move_cmd << curr_tick << x_dir << y_dir) {
+      realtime_client->send(packet,"127.0.0.1", 4845);
+    } else {
+      std::cout << "Failed to form packet" << std::endl;
+    }
+    std::this_thread::sleep_for(std::chrono::milliseconds(80));
+  }
+}
+
+// this probably should just be in realtime_client_send and realtime_client_send
+// should poll client state to see if the current tick has a move AND if the move has
+// been sent to the server yet. If both are true, then it should try to communicate the move.
+// This would decouple local move setting commands from the network communication. They'd be happening
+// in different threads, which is a Good Thing(tm) to avoid blocking in the client on network IO.
+void sync_server_state(sf::UdpSocket* realtime_client) {
+  while (true) {
+    sf::Packet packet;
+    sf::Uint32 fetch_state_cmd = (sf::Uint32)RealtimeCommand::fetch_state;
+    if (packet << my_client_id << fetch_state_cmd << curr_tick) {
+      realtime_client->send(packet, "127.0.0.1", 4845);
+    } else {
+      std::cout << "Failed to form packet" << std::endl;
+    }
+    // fetch state constantly
+    std::this_thread::sleep_for(std::chrono::milliseconds(50));
   }
 }
 
 int main(int, char const**)
 {
-  // not a great way to do this, server should dispense these or use a UUID lib
-  std::srand(std::time(nullptr));
-  my_group_id = std::rand();
+  std::cout << "Starting ungroup demo client." << std::endl;
+  sf::TcpSocket api_client;
+  api_client.connect("127.0.0.1", 4844);
+  sf::Packet registration_request;
+  if(registration_request << (sf::Uint32)APICommand::register_client) {
+    api_client.send(registration_request);
+    read_registration_response(&api_client);
+  }
 
-  sf::TcpSocket socket;
-  socket.connect("127.0.0.1", 4844);
+  if (!is_registered) {
+    std::cout << "Failed to register. Exiting." << std::endl;
+    return EXIT_FAILURE;
+  }
 
-  std::cout << "Connected. Starting threads.\n";
+  sf::UdpSocket realtime_client;
+  realtime_client.bind(4846);
+/*
+  // api
+  std::thread api_client_recv_thread(api_client_recv, &api_client);
+  std::thread api_client_send_thread(api_client_send, &api_client);
+*/
+  // realtime
+  std::thread realtime_client_recv_thread(realtime_client_recv, &realtime_client);
+  std::thread realtime_client_send_thread(realtime_client_send, &realtime_client);
 
-  std::mutex mutex;
-  std::thread t1(recv, std::ref(socket), std::ref(mutex));
-  std::thread t2(send, std::ref(socket), std::ref(mutex));
-  t1.join();
-  t2.join();
+  // syncs authoritative sever state to client at a regular interval
+  std::thread sync_server_state_thread(sync_server_state, &realtime_client);
+
+  // I don't really know if all these joins do anything if the first thread I join is in an infinite loop
+  // api_client_recv_thread.join();
+  // api_client_send_thread.join();
+  realtime_client_recv_thread.join();
+  realtime_client_send_thread.join();
+  sync_server_state_thread.join();
 
   return EXIT_SUCCESS;
 }
--- src/server/main_server.cpp
+++ src/server/main_server.cpp
@@ -2,35 +2,26 @@
 
 #include <iostream>
 #include <list>
+#include <thread>
+#include <future>
+#include <chrono>
+#include <unordered_map>
 
-sf::Uint32 curr_tick;
+const unsigned int CMD_DRIFT_THRESHOLD = 5;
+enum class APICommand { register_client, group, ungroup };
+enum class RealtimeCommand { move, fetch_state };
 
-void send(std::list<sf::TcpSocket*> clients)
-{
-  for( auto client:clients )
-  {
-    sf::Packet packet;
-    curr_tick++;
-    sf::Uint32 tick = curr_tick;
-    // these values must be replaced with the authoritative view of the current clients position.
-    // we probably need to send the state of all other clients to every single client also n^2ish
-    sf::Uint32 group_id = 7331;
-    sf::Uint32 x_pos = 1337;
-    sf::Uint32 y_pos = 4484;
-    sf::Uint32 x_dir = 0;
-    sf::Uint32 y_dir = 0;
-    if (packet << group_id << tick << x_dir << y_dir << x_pos << y_pos)
-    {
-      std::cout << "Sending group_id:" << group_id << " tick:" << tick << " x_dir:" << x_dir << " y_dir:" << y_dir << " x_pos:" << x_pos << " y_pos:" << y_pos << "\n";
-      client->send(packet);
-    }
-  }
-}
+std::unordered_map<sf::Uint32, sf::Int32*> client_moves;
+std::unordered_map<sf::Uint32, sf::Uint32*> client_positions;
+sf::Uint32 client_id_counter = 1000;
 
-int main(int, char const**)
-{
+std::atomic<uint> curr_tick(0);
+std::atomic<bool> accepting_move_commands(true);
+
+void api_server() {
   // Create a socket to listen to new connections
   sf::TcpListener listener;
+  // API socket
   listener.listen(4844);
 
   // Create a list to store the future clients
@@ -55,7 +46,6 @@ int main(int, char const**)
         sf::TcpSocket* client = new sf::TcpSocket;
         if (listener.accept(*client) == sf::Socket::Done)
         {
-          std::cout << "New client added!\n";
           // Add the new client to the clients list
           clients.push_back(client);
           // Add the new client to the selector so that we will
@@ -76,18 +66,16 @@ int main(int, char const**)
             sf::Packet packet;
             if (client.receive(packet) == sf::Socket::Done)
             {
-              // Extract the variables contained in the packet
-              sf::Uint32 group_id;
-              sf::Uint32 tick;
-              sf::Uint32 x_dir;
-              sf::Uint32 y_dir;
-              sf::Uint32 x_pos;
-              sf::Uint32 y_pos;
-              std::cout << "Received a packet!\n";
-              if (packet >> group_id >>  tick >> x_dir >> y_dir >> x_pos >> y_pos)
-              {
-                  std::cout << group_id << " " << tick << " " << x_dir << " " << y_dir << " " << x_pos << " " << y_pos << "\n";
-                  send(clients);
+              sf::Uint32 api_command;
+              if (packet >> api_command && (api_command == (sf::Uint32)APICommand::register_client)) {
+                sf::Packet response_packet;
+                sf::Uint32 api_command = (sf::Uint32)APICommand::register_client;
+                if(response_packet << api_command << client_id_counter << (sf::Uint32)curr_tick) {
+                  client.send(response_packet);
+                  std::cout << "Received client registration. Issued client ID " << client_id_counter << std::endl;
+                  client_positions[client_id_counter] = new sf::Uint32[2]{0, 0};
+                  client_id_counter++;
+                }
               }
             }
           }
@@ -95,5 +83,111 @@ int main(int, char const**)
       }
     }
   }
+}
+
+void realtime_server() {
+  sf::UdpSocket rt_server;
+  rt_server.bind(4845);
+
+  while (true) {
+    sf::Uint32 ct = curr_tick;
+    sf::Packet game_state_packet;
+    sf::Packet command_packet;
+    sf::IpAddress sender;
+    unsigned short port;
+    rt_server.receive(command_packet, sender, port);
+
+    // Extract the variables contained in the command_packet
+    sf::Uint32 client_id;
+    sf::Uint32 tick;
+    sf::Uint32 realtime_command;
+    if (command_packet >> client_id >> realtime_command >> tick) {
+      switch(realtime_command) {
+        case (sf::Uint32)RealtimeCommand::move:
+          sf::Int32 x_dir;
+          sf::Int32 y_dir;
+          if (command_packet >> x_dir >> y_dir) {
+            std::cout << "Client ID, tick, command, x, y: " << client_id << " " << tick << " " << realtime_command << " " << x_dir << " " << y_dir << std::endl;
+            int drift = std::abs((int)((curr_tick - tick)));
+            if (drift < CMD_DRIFT_THRESHOLD) {
+              // your newest commands will overwrite old ones
+              std::unordered_map<sf::Uint32, sf::Int32*>::const_iterator iter = client_moves.find(client_id);
+              if (accepting_move_commands) {
+                // if entry exists, don't allocate new array.
+                if(iter != client_moves.end()) {
+                  iter->second[0] = x_dir;
+                  iter->second[1] = y_dir;
+                }
+                // drop the move command if we're computing game state in the other thread. doesn't feel great.
+                client_moves[client_id] = new sf::Int32[2]{x_dir, y_dir};
+              } else {
+                std::cout << "Dropping move command because game state being computed" << std::endl;
+              }
+            } else {
+              std::cout << "Receive move command with tick drifted past drift threshold. Drift value is: " << drift << std::endl;
+            }
+          }
+          break;
+        case (sf::Uint32)RealtimeCommand::fetch_state:
+          std::cout << "Sending game state to client: " << sender << " " << port << std::endl;
+          // sample current state every 100 ms, this simply packages and returns it
+          game_state_packet << ct; // should have error handling for <<
+          for (const auto iter : client_positions) {
+            sf::Uint32 client_id = iter.first;
+            game_state_packet << client_id << iter.second[0] << iter.second[1];
+          }
+          rt_server.send(game_state_packet, "127.0.0.1", 4846);
+          break;
+        default:
+          std::cout << "Unknown command code sent: " << realtime_command << std::endl;
+          break;
+      }
+    }
+  }
+}
+
+// add instrumentation to make sure this is actually running near every 100 ms
+void compute_game_state() {
+  while (true) {
+    accepting_move_commands = true;
+    std::this_thread::sleep_for(std::chrono::milliseconds(100));
+    accepting_move_commands = false;
+    // update game state, take moves array and simulate next tick given current state and inputs
+    // ... simplest behavior would be to track client locations and apply vectors to them ...
+
+    if (client_moves.size() == 0) {
+      continue;
+    }
+    std::cout << "game state thread: dumping client moves for tick " << curr_tick << std::endl;
+    for (const auto iter : client_moves) {
+      sf::Uint32 client_id = iter.first;
+      sf::Int32* move_vector = iter.second;
+      if (client_positions.find(client_id) != client_positions.end()) {
+        client_positions[client_id][0] += move_vector[0]; // x component
+        client_positions[client_id][1] += move_vector[1]; // y component
+      }
+      std::cout << iter.first << " " << iter.second[0] << " " << iter.second[1] << std::endl;
+    }
+    std::cout << "game state thread: dumping client positions for tick " << curr_tick << std::endl;
+    for (const auto iter : client_positions) {
+      std::cout << iter.first << " " << iter.second[0] << " " << iter.second[1] << std::endl;
+    }
+    // window is closed, time to reset. May want to use fix sized array as an optimization.
+    // It's a bit mean to disguard old commands, maybe there should be windows for multiple ticks?
+    client_moves.clear();
+    curr_tick++;
+  }
+}
+
+int main(int, char const**)
+{
+  std::cout << "Starting ungroup game server.\n";
+  std::thread api_thread(api_server);
+  std::thread realtime_thread(realtime_server);
+  std::thread compute_game_state_thread(compute_game_state);
+  api_thread.join();
+  realtime_thread.join();
+  compute_game_state_thread.join();
+
   return EXIT_SUCCESS;
 }
