2ca6c5e482e5577e5575ea41fcbc724b1ef597a6
souren <souren@papazian.info>
2019-06-26 17:53:28 -0700

    Minor fix

--- src/common/network_util.hpp
+++ src/common/network_util.hpp
@@ -13,7 +13,7 @@ sf::Packet& operator >>(sf::Packet& packet, ApiCommand& api_command);
 sf::Packet& operator <<(sf::Packet& packet, const sf::Vector2f& vector_2f);
 sf::Packet& operator >>(sf::Packet& packet, sf::Vector2f& vector_2f);
 
-sf::Packet& operator <<(sf::Packet& packet, const ClientGroupUpdate& vector_2f);
-sf::Packet& operator >>(sf::Packet& packet, ClientGroupUpdate& vector_2f);
+sf::Packet& operator <<(sf::Packet& packet, const ClientGroupUpdate& client_group_update);
+sf::Packet& operator >>(sf::Packet& packet, ClientGroupUpdate& client_group_update);
 
 #endif /* network_util_hpp */
