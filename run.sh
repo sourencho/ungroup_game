9cb1caf69148c980da8b3fa838927906ca926c42
Hayg Astourian <haygastourian@gmail.com>
2019-12-11 00:37:24 -0800

    Switch run.sh to using build.sh

--- run.sh
+++ run.sh
@@ -1,14 +1,6 @@
 #!/bin/bash
 
-if [[ $(which ninja) ]]; then
-    echo "Detected Ninja is installed. Using Ninja."
-    ninja -C build
-else
-    echo "Ninja not found. Using CMake directly."
-    mkdir build
-    cmake -S . -B build
-    cmake --build build -- -j
-fi
+./build.sh
 
 # You can exit all tmux windows at once by pressing CTRL+B and typing :kill-window
 
