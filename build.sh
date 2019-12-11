b14eaaecff8af344a619ccb20c02f5d333d13ca7
souren <souren@papazian.info>
2019-12-11 16:53:40 +0400

    Improve build script

--- build.sh
+++ build.sh
@@ -2,10 +2,10 @@
 
 if [[ $(which ninja) ]]; then
     echo "Detected Ninja is installed. Using Ninja."
+    cmake -GNinja -S . -B build
     ninja -C build
 else
     echo "Ninja not found. Using CMake directly."
-    mkdir build
     cmake -S . -B build
     cmake --build build -- -j
 fi
