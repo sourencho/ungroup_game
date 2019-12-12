a22aa985daf2012e08d63759f0abc6fd00149b81
souren <souren@papazian.info>
2019-12-12 11:04:37 +0400

    Set number of clients via command line parameter for run.sh

--- run.sh
+++ run.sh
@@ -1,16 +1,22 @@
 #!/bin/bash
+# You can exit all tmux windows at once by pressing CTRL+B and typing :kill-window
 
 ./build.sh
 
-# You can exit all tmux windows at once by pressing CTRL+B and typing :kill-window
+if [ "$1" != "" ]; then
+    CLIENT_COUNT=$1
+else
+    CLIENT_COUNT=2
+fi
 
 tmux new -s ungroup -d
 tmux rename-window -t ungroup game
 tmux send-keys -t ungroup './build/src/server/ug-server' C-m
-tmux split-window -v -t ungroup
-tmux send-keys -t ungroup './build/src/client/ug-client' C-m
-tmux split-window -v -t ungroup
-tmux send-keys -t ungroup './build/src/client/ug-client' C-m
+for (( i = 0; i < $CLIENT_COUNT; i++ ))
+do
+    tmux split-window -v -t ungroup
+    tmux send-keys -t ungroup './build/src/client/ug-client' C-m
+done
 tmux select-window -t ungroup:1
 tmux attach -t ungroup
 
